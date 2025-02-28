c8fb0693e228af64c8ff7aa7d795fef93607bd12
Tunamu <tunamuswork@gmail.com>
2025-02-28 14:35:21 +0100

    Update SignUpPage.js

--- client/src/SignUpPage.js
+++ client/src/SignUpPage.js
@@ -2,20 +2,30 @@ import { useState } from 'react';
 import './SignUp.css'
 
 function SignUpPage({setEmail}) {
+  const [inputName, setInputName] = useState("");
+  const [inputSurname, setInputSurname] = useState("");
   const [inputEmail, setInputEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+
+  function SignUpFunc(){
+    if(password!=confirmPassword){
+      setErrorMessage("Password and Confirm Password are not match. Try Again");
+      return;
+    }
+  }
 
   return (
     <div className="LogPage">
       <h2>SignUp</h2>
       {errorMessage && <p>{errorMessage}</p>}
-      <input type="name" onChange={(e) => setInputEmail(e.target.value)} placeholder="Name" />
-      <input type="surname" onChange={(e) => setInputEmail(e.target.value)} placeholder="Surname" />
+      <input type="name" onChange={(e) => setInputName(e.target.value)} placeholder="Name" />
+      <input type="surname" onChange={(e) => setInputSurname(e.target.value)} placeholder="Surname" />
       <input type="email" onChange={(e) => setInputEmail(e.target.value)} placeholder="Email" />
       <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Confirm Password" />
-      <button className="Submit-Buttons" onClick={""}>SignUp</button>
+      <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm Password" />
+      <button className="Submit-Buttons" onClick={SignUpFunc}>SignUp</button>
     </div>
   );
 }
