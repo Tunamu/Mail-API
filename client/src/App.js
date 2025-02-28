010febed15cad506d8c1140d7150c2c1a8155388
Tunamu <tunamuswork@gmail.com>
2025-02-28 16:27:02 +0100

    added header logo and text for main website

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import LogPage from "./LogPage";
 import MainPage from "./MainPage";
+import logoImage from './img/logo.png'
 
 function App() {
   const [email, setEmail] = useState(localStorage.getItem("email") || "");
@@ -20,6 +21,10 @@ function App() {
 
   return (
     <div className="App">
+      <div className="Top-Part">
+        <img src={logoImage} className="Logo-Photo"/>
+        <p className="Top-Text">yippee.com - a basic mail application.</p>
+      </div>
       {email ? <MainPage setEmail={setEmail} /> : <LogPage setEmail={setEmail} />}
     </div>
   );
