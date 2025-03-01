3ec910ba8e499a89412c9654ae6b721660445a3f
Tunamu <tunamuswork@gmail.com>
2025-03-01 23:00:39 +0100

    Added new mail page template

--- client/src/NewMail.js
+++ client/src/NewMail.js
@@ -5,8 +5,14 @@ function NewMail({setNewMessage}) {
 return (
     <div className="NewMail">
         <div className="New-Mail-Part">
-            Hello
-            <button onClick={()=>setNewMessage(false)}>X</button>      
+            <div className="Top-Line">
+                <input className="New-Mail-Header" placeholder="Header"/>
+                <button className="Exit-Button" onClick={()=>setNewMessage(false)}>X</button>      
+            </div>
+            <input className="New-Mail-Receiver-Mail-Adress" placeholder="Email Adress"/>
+            <input className="New-Mail-Topic" placeholder="Topic"/>
+            <textarea className="New-Mail-Text" placeholder="write here..."/>
+            <button className="Mail-Send-Button">Send</button>
         </div> 
     </div>
     );
